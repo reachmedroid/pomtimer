de4baa622e13d56200e8288fb585d8f4e95c1df9
unknown <Angel@.(none)>
2011-08-24 15:16:21 +0100

    Initial commit

---
+++ src/main/pomtimer/PomtimerActivity.java
@@ -0,0 +1,80 @@
+package main.pomtimer;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PomtimerActivity extends Activity implements OnClickListener {
+	int longDuration;
+	int shortDuration;
+	
+	static String LONG_DURATION = "longDuration";
+	static String SHORT_DURATION = "shortDuration";
+	
+	public void setLongDuration(int longDuration) {
+		this.longDuration = longDuration;
+	}
+	
+	public void setShortDuration(int shortDuration) {
+		this.shortDuration = shortDuration;
+	}
+	
+	public int getLongDuration() {
+		return longDuration;
+	}
+	
+	public int getShortDuration() {
+		return shortDuration;
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Log.v("pomtimer", "content view has been set");
+        
+        // set click listener to button
+        Button updateButton = (Button)findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(this);
+        
+        // restore preferences
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        int longDuration = settings.getInt(LONG_DURATION, 0);
+        int shortDuration = settings.getInt(SHORT_DURATION, 0);
+        
+        this.setLongDuration(longDuration);
+        this.setShortDuration(shortDuration);
+    }
+    
+    private void saveSettings() {
+    	SharedPreferences settings = getPreferences(MODE_PRIVATE);
+    	SharedPreferences.Editor editor = settings.edit();
+    	editor.putInt(LONG_DURATION, this.longDuration);
+    	editor.putInt(SHORT_DURATION, this.shortDuration);
+    	editor.commit();
+    }
+    
+    @Override
+    protected void onStop() {
+    	saveSettings();
+    	super.onStop();
+    }
+	
+	public void onClick(View v) {
+		// get values entered into text fields
+		EditText longTextView = (EditText) findViewById(R.id.longText);
+		EditText shortTextView = (EditText) findViewById(R.id.shortText);
+		
+		// set them to class variables
+		setLongDuration(Integer.parseInt(longTextView.getText().toString()));
+		setShortDuration(Integer.parseInt(shortTextView.getText().toString()));
+		
+	}
+}
